b1bcc1d4e7f6e1a5a0a74e5fbd195dada6107662
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-23 11:13:44 +0200

    dev: added 3 components

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BodyComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BodyComponent,
+    FooterComponent,
+    HeaderComponent
   ]
 })
 export class LayoutModule { }
