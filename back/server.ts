faa76579b7515634806010ae9aa98a5fec3faea5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-24 16:22:48 +0200

    prod: added angularvdir to back-end

--- back/server.ts
+++ back/server.ts
@@ -1,16 +1,21 @@
 import express from "express";
 import serveIndex from "serve-index";
 import cors from "cors";
+import path from "path";
 
 import { api } from "./api-mongo";
 
 const app = express();
 const port = +(process.env.GSTOCK_PORT || "3000");
 const publicDir = process.env.GSTOCK_DIR || "./public";
+const angularDir = path.resolve(process.cwd(), "../front/dist/front");
 
 app.use(cors());
 
 app.use("/api", api);
+
+app.use(express.static(angularDir));
+app.use(serveIndex(angularDir));
 
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir));
