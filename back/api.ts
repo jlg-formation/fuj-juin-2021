a9d753b6ebe9bd551fd58f749ad1b6660e23c005
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-24 11:33:01 +0200

    dev: remove http

--- back/api.ts
+++ back/api.ts
@@ -4,10 +4,12 @@ import { Article } from "../front/src/app/interfaces/article";
 const app = express.Router();
 export const api = app;
 
-const articles: Article[] = [
-  { name: "Tournevis", price: 2.99, qty: 234 },
-  { name: "Marteau", price: 5.26, qty: 12 },
+let articles: Article[] = [
+  { id: "a1", name: "Tournevis", price: 2.99, qty: 234 },
+  { id: "a2", name: "Marteau", price: 5.26, qty: 12 },
 ];
+
+let seq = 2;
 
 app.get("/articles", (req, res) => {
   res.json(articles);
@@ -17,6 +19,14 @@ app.use(express.json());
 
 app.post("/articles", (req, res) => {
   const article = req.body as Article;
+  seq++;
+  article.id = "a" + seq;
   articles.push(article);
   res.json(article);
+});
+
+app.delete("/articles", (req, res) => {
+  const ids = req.body as string[];
+  articles = articles.filter((a) => !ids.includes(a.id));
+  res.status(204).end();
 });
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ArticleService } from './article.service';
 import { Article } from '../interfaces/article';
 
@@ -32,6 +32,29 @@ export class HttpArticleService extends ArticleService {
     super.add(article);
     this.http
       .post<void>('http://localhost:3000/api/articles', article)
+      .subscribe({
+        next: () => {
+          this.refresh();
+        },
+        complete: () => {
+          console.log('complete');
+        },
+        error: (err) => {
+          console.error('err: ', err);
+        },
+      });
+  }
+
+  remove(selectedArticles: Set<Article>) {
+    super.remove(selectedArticles);
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      body: [...selectedArticles].map((a) => a.id),
+    };
+    this.http
+      .delete<void>('http://localhost:3000/api/articles', options)
       .subscribe({
         next: () => {
           this.refresh();
