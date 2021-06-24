7476badd77b37f98979fbd7de0b953d1a73d43dd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-24 09:26:39 +0200

    dev: added selected articles

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 import { ArticleService } from '../services/article.service';
 
 @Component({
@@ -7,7 +8,18 @@ import { ArticleService } from '../services/article.service';
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  selectedArticles = new Set<Article>();
+
   constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  toggle(a: Article) {
+    console.log('toggle', a);
+    if (this.selectedArticles.has(a)) {
+      this.selectedArticles.delete(a);
+      return;
+    }
+    this.selectedArticles.add(a);
+  }
 }
