6ac57f829c9ea06c48d80cefb511f71d263bd233
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-06-25 15:07:17 +0200

    test: index.ts

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,32 +1,8 @@
-import express from 'express';
-import serveIndex from 'serve-index';
-import cors from 'cors';
-import path from 'path';
-
-import {api} from './api-mongo';
+import {WebServer} from './WebServer';
 
 console.log('starting...');
 
-const app = express();
 const port = +(process.env.GSTOCK_PORT || '3000');
-const publicDir =
-  process.env.GSTOCK_DIR || path.resolve(process.cwd(), './public');
-const angularDir = path.resolve(process.cwd(), '../front/dist/front');
 
-app.use(cors());
-
-app.use('/api', api);
-
-app.use(express.static(angularDir));
-app.use(serveIndex(angularDir));
-
-app.use(express.static(publicDir));
-app.use(serveIndex(publicDir));
-
-app.use((req, res) => {
-  res.sendFile(path.resolve(angularDir, 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const server = new WebServer(port);
+server.start();
